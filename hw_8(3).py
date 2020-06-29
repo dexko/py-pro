12eaedabdb7e2de2cc5390a4072a6d3c46646c60
Viktoria Milshina <dexko26@gmail.com>
2020-06-29 23:48:14 +0300

    Update hw_8(3).py

--- hw_8(3).py
+++ hw_8(3).py
@@ -1,11 +1,66 @@
-3
+#1
+import re
+EMAIL_REGEX = re.compile( r"[A-Za-z0-9-_]+(.[A-Za-z0-9-_]+)*@""[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})")
 
-class IngegerField:
-    def __init__(self, number):
-        self.number = number  # (4)
+
+def ValidateEmail(email_address):
+    if not EMAIL_REGEX.match(email_address):
+        raise ValueError
+
+
+class EmailDescriptor:
+    """Descriptor class used to validate an email address."""
+
+    def __init__(self):
+        self.email = {}
 
     def __get__(self, instance, owner):
-        return instance.__dict__[self.number]  # (5)
+        return self.email.get(instance, 0)
+
+    def __set__(self, instance, email_address):
+        if not EMAIL_REGEX.match(email_address):
+            raise ValueError
+        self.email[instance] = email_address
+
+
+class MyClass:
+    email = EmailDescriptor()
+
+
+my_class = MyClass()
+my_class.email = "validemail@gmail.com"
+
+my_class.email = "novalidemail"
+#2
+
+class Singleton(type):
+    _instances={}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in Singleton._instances:
+            Singleton._instances[cls] = super().__call__(*args, **kwargs)
+        return Singleton._instances[cls]
+
+
+class MyClass(metaclass=Singleton):
+    pass
+
+
+c = MyClass()
+b = MyClass()
+assert id(c) == id(b)
+
+
+#3
+
+class IngegerField:
+    """Descriptor class that hold unicue states ."""
+
+    def __init__(self, number):
+        self.number = number  
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.number]  
 
     def __set__(self, instance, value):
         instance.__dict__[self.number] = value
